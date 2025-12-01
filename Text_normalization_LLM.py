7ee22044f96130c530c5889874df9d57a2b50dc2
nitajadav18 <nitajadav8@gmail.com>
2025-12-01 15:32:18 +0530

    Update Text_normalization_LLM.py

--- Text_normalization_LLM.py
+++ Text_normalization_LLM.py
@@ -29,26 +29,8 @@ Document:
     return response.choices[0].message.content
 
 
-
-def summarize(doc):
-    prompt = f"""
-You are a abstractive text summarizer for legal document in Indian legal domain
--Generate a coherence summary from the given document.
--Return the result as a list of maximum 3 consistent sentences.
-Document:
-{doc}
-"""
-
-    response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=[{"role": "user", "content": prompt}],
-        temperature=0
-    )
-
-    return response.choices[0].message.content
-
 def main():
-    parser = argparse.ArgumentParser(description="Summarize paragraph CSV file.")
+    parser = argparse.ArgumentParser(description="Normalize Document.")
     parser.add_argument("--input", required=True, help="Path to input CSV ")
     parser.add_argument("--output", required=True, help="Path to output CSV")
     args = parser.parse_args()
@@ -59,14 +41,14 @@ def main():
     for _, row in val.iterrows():
         doc_id = row["doc_id"]
         doc = row["para_text"]
-        summaries = summarize(doc)
+        Normal_para =  split_into_paragraphs(doc)
 
         for i, s in enumerate(summaries):
             rows.append({
                 "doc_id": doc_id,
                 "para_id": i,
                 "para_text": doc,
-                "summary": s
+                "Normal_para":Normal_para
             })
 
     out_df = pd.DataFrame(rows)
